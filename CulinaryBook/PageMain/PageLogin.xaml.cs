5e6683921d9a221d20e1124c553b37c756886b93
dneklyudov <dn@verdin.ru>
2024-10-19 00:17:07 +0300

    Add project files.

---
+++ CulinaryBook/PageMain/PageLogin.xaml.cs
@@ -0,0 +1,49 @@
+﻿using CulinaryBook.ApplicationData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CulinaryBook.PageMain
+{
+    /// <summary>
+    /// Interaction logic for PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppConnect.model0db.Authors.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == psbPassword.Password);
+                if (userObj == null) 
+                {
+                    MessageBox.Show("Такой пользователь не найден", "Ошибка при авторизации", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Привет, " + userObj.AuthorName, "Успешная авторизация", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка " + Ex.Message.ToString(), "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
